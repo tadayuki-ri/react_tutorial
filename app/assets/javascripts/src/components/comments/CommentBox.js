5ce79f05997d78699873467ebd8346e646f85b54
somaakihiro <aki.air@icloud.com>
2016-06-25 15:40:33 +0900

    微修正

--- app/assets/javascripts/src/components/comments/CommentBox.js
+++ app/assets/javascripts/src/components/comments/CommentBox.js
@@ -1,27 +1,6 @@
 import React from 'react'
 
 export default class CommentBox extends React.Component {
-
-  static get defaultProps() {
-    return {
-    }
-  }
-
-  static get propTypes() {
-    return {
-    }
-  }
-
-  constructor(props) {
-    super(props)
-    this.state = this.initialState
-  }
-
-  get initialState() {
-    return {
-    }
-  }
-
   render() {
     return (
       <div>
